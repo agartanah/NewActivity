0e4ce30f102ade3994c6034aea2bd7e83a669585
agartanah <vyatin2017@mail.ru>
2023-12-09 08:45:34 +0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/newactivity/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.newactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private val URL = "http://abload.de/img/gandalf-laughhugeylo0t.gif"
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, PicActivity::class.java)
+            intent.putExtra(R.string.key_link.toString(), URL)
+            startActivity(intent)
+        }
+    }
+}
